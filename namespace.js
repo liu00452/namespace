3ea70949c25b5acb898c6f25fb2b410162b10ecd
abigail <liu00452@algonquinlive.com>
2018-10-24 18:04:43 -0400

    change

--- namespace.js
+++ namespace.js
@@ -5,33 +5,33 @@ Description:
 Date: 2018-10-24
 *********************************/
 var LIU00452 = {
-    init : function init(){
-    let div = document.createElement("div");
-    div.className = "box";
-    div.textContent = "liu00452";
-    
-    let boxes = document.getElementById("boxes");
-    boxes.appendChild(div);
-   
+    init: function init() {
+        let div = document.createElement("div");
+        div.className = "box";
+        div.textContent = "liu00452";
 
-    div.addEventListener("click", LIU00452.Click);
-    div.addEventListener("mouseover", LIU00452.mouseOver);
-    div.addEventListener("mouseout", LIU00452.mouseOut);  
-},
-    Click : function() {
-    this.style.borderColor = "pink";
-    this.style.backgroundColor = "gray";
-    console.log(this);
-},
+        let boxes = document.getElementById("boxes");
+        boxes.appendChild(div);
 
-    mouseOver : function() {
-    this.classList.toggle("highlight");
-    console.log(this);
-},
 
-     mouseOut : function () {
-  this.classList.toggle("highligght");
-  this.removeAttribute('style'); 
-  console.log(this);
-}
+        div.addEventListener("click", LIU00452.Click);
+        div.addEventListener("mouseover", LIU00452.mouseOver);
+        div.addEventListener("mouseout", LIU00452.mouseOut);
+    },
+    Click: function () {
+        this.style.borderColor = "pink";
+        this.style.backgroundColor = "gray";
+        console.log(this);
+    },
+
+    mouseOver: function (e) {
+        e.target.classList.toggle("highlight");
+        console.log(e);
+    },
+
+    mouseOut: function (e) {
+        e.target.classList.toggle("highligght");
+        e.target.removeAttribute('style');
+        console.log(e);
+    }
 };
